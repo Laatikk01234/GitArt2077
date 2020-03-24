3503bbff4f2684f0cb2c67351111723d8ef36e38
Laatikk01234 <Jyry.uitto@gmail.com>
2020-03-24 11:56:18 +0200

    Add project

---
+++ Art 2077/Assets/Scripts/Artinformation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artinformation : MonoBehaviour
+{
+    public Sprite Picture;
+    public int pictureValue;
+    public List<string> traineeAnswers;
+    public List<string> professorAnswers;
+    // miten vastauksia?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Art 2077/Assets/Scripts/Gamelogic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gamelogic : MonoBehaviour
+{
+    public List<GameObject> art;
+
+    public GameObject trainee;
+    private TextMeshPro traineeText;
+
+    public GameObject professor;
+    private TextMeshPro professorText;
+
+    public Slider artValueSlider;
+
+    public GameObject sliderValue;
+    private TextMeshPro sliderValueText;
+    // artValueSlider.value -> pitääkö vielä ajaa slideristä changevalueta?
+    public Button confirm; 
+
+    private void Start()
+    {
+        traineeText = trainee.GetComponent<TextMeshPro>();
+
+        professorText = professor.GetComponent<TextMeshPro>();
+
+        sliderValueText = sliderValue.GetComponent<TextMeshPro>();
+
+        Button btn = confirm.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    public void sliderValueChange()
+    {
+        string message = artValueSlider.value + " Dollars";
+        sliderValueText.text = message;
+        
+    }
+    // both of these work for clicking
+    void TaskOnClick()
+    {
+        // ok clicked -> give response and then change art piece
+
+    }
+
+
+
+
+}
