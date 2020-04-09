d4fdde3e53507cc488adbe483b0ff6bb1d469e2d
Laatikk01234 <Jyry.uitto@gmail.com>
2020-04-09 10:59:11 +0300

    Working main menu

---
+++ Art 2077/Assets/Scripts/MainMenuLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    
+    public void Play()
+    {
+        SceneManager.LoadScene("Main Game");
+    }
+
+    public void Credits()
+    {
+        // or popup ui?
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
